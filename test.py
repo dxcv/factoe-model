807f5c845d40fb71d7685bfd1d928ba7f51f8d16
randomtutu <563056419@qq.com>
2019-10-17 10:53:58 +0800

    test

---
+++ test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'B': [5, 6, 7, 8, 9],
+                   'C': ['a', 'b', 'c', 'd', 'e']})
+df = df.replace({'A': 0},'sss')
+print(df)
